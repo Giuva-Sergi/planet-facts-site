be0c23b6c1c4d0a042548677ccf5390f2baf78c0
Giovanni Sergi <giovanni.srg@gmail.com>
2024-03-13 16:54:00 +0100

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,12 @@ import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   const planets = data;
 
-  // const randomPlanet = planets.at(
-  //   Math.floor(Math.random() * planets.length)
-  // ).name;
-
   return (
     <>
       <Navbar planets={planets} />
       <Routes>
         <Route path="/" element={<Navigate to={`/${planets.at(2).name}`} />} />
-        {planets.map((planet) => (
-          <Route
-            key={planet.name}
-            path={`/${planet.name}`}
-            element={<PlanetPage planet={planet} />}
-          />
-        ))}
-        {/* <Route path="/:planet" element={<PlanetPage planets={planets} />} /> */}
+        <Route path="/:planet" element={<PlanetPage planets={planets} />} />
       </Routes>
     </>
   );
