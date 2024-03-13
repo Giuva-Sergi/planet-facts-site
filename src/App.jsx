7512c22505fa050e541e66beee3145cd0667382c
Giovanni Sergi <giovanni.srg@gmail.com>
2024-03-13 16:52:42 +0100

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,23 @@ import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   const planets = data;
 
-  const randomPlanet = planets.at(
-    Math.floor(Math.random() * planets.length)
-  ).name;
+  // const randomPlanet = planets.at(
+  //   Math.floor(Math.random() * planets.length)
+  // ).name;
 
   return (
     <>
       <Navbar planets={planets} />
       <Routes>
-        <Route path="/" element={<Navigate to={`/${randomPlanet}`} />} />
-        <Route path="/:planet" element={<PlanetPage planets={planets} />} />
+        <Route path="/" element={<Navigate to={`/${planets.at(2).name}`} />} />
+        {planets.map((planet) => (
+          <Route
+            key={planet.name}
+            path={`/${planet.name}`}
+            element={<PlanetPage planet={planet} />}
+          />
+        ))}
+        {/* <Route path="/:planet" element={<PlanetPage planets={planets} />} /> */}
       </Routes>
     </>
   );
