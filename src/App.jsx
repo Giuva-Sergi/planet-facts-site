65072aebd1a5935e398962a772ffe17fe0b5f231
Giovanni Sergi <giovanni.srg@gmail.com>
2024-03-13 16:55:03 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import { Route, Routes, Navigate } from "react-router-dom";
 function App() {
   const planets = data;
 
+  const randomPlanet = planets.at(
+    Math.floor(Math.random() * planets.length)
+  ).name;
+
   return (
     <>
       <Navbar planets={planets} />
       <Routes>
-        <Route path="/" element={<Navigate to={`/${planets.at(2).name}`} />} />
+        <Route path="/" element={<Navigate to={`/${randomPlanet}`} />} />
         <Route path="/:planet" element={<PlanetPage planets={planets} />} />
       </Routes>
     </>
